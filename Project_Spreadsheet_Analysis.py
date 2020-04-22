46ab9cef0a85e782c2b3858668f692b9781c76e7
Ronja <ronja@Olafurs-MacBook-Pro.local>
2020-04-22 19:50:13 +0000

    Make program runnable

--- Project_Spreadsheet_Analysis.py
+++ Project_Spreadsheet_Analysis.py
@@ -8,15 +8,16 @@ def read_file():
         spreadsheet = csv.DictReader(csv_file)
 
 # Required task 2. Collect all of the sales from each month into a single list
-    total_sales = []
+        total_sales = []
 
-    for row in spreadsheet:
-        monthly_sales = row['sales']
-        total_sales.append(int(monthly_sales))
+        for row in spreadsheet:
+            monthly_sales = row['sales']
+            total_sales.append(int(monthly_sales))
 
 # Required task 3. Output the total sales across all months
-name = 'London Milan'
 
-    print(f'The total sales across all {len(total_sales)} months are {sum(total_sales)} {name}.')
+        name = 'London'
+        print(f'The total sales across all {len(total_sales)} months are {sum(total_sales)} {name}.')
 
+read_file()
 
